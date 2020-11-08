b58825234c7be6a8999533efa714aef749c4d627
cyberpayload <will@cyberpayload.com>
2020-11-07 19:07:08 -0600

    added math portion operations

--- calc.py
+++ calc.py
@@ -13,4 +13,18 @@ print('')
 # capture the operation input from the user
 operation = int(input('Type 1/2/3/4\n'))
 
+
 # check the operation input from the user and perform the math
+while True
+    if operation == 1:
+        num1 + num2
+    elif operation == 2:
+        num1 - num2
+    elif operation == 3:
+        num1 * num2
+    elif operation == 4:
+        num1 / num2
+    return
+
+    else:
+        print('Invalid Choice')
