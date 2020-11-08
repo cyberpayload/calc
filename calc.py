30adb1f6a13fbed777e35a926888c5a9c9425026
cyberpayload <will@cyberpayload.com>
2020-11-07 18:48:05 -0600

    added commentsto depict sections of program

---
+++ calc.py
@@ -0,0 +1,15 @@
+# ********** A SIMPLE CALUCLATOR PROGRAM WITH NO USER INTERFACE **********
+
+
+
+# Display an operation menu to the user
+
+
+
+
+# capture the operation input from the user
+
+
+
+
+# check the operation input from the user and perform the math
