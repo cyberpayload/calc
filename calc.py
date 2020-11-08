af88a3a6d18075161d18c36866495de553dff29d
cyberpayload <will@cyberpayload.com>
2020-11-07 19:22:03 -0600

    finished basic operation calculator

--- calc.py
+++ calc.py
@@ -1,7 +1,6 @@
 # ********** A SIMPLE CALUCLATOR PROGRAM WITH NO USER INTERFACE **********
 
 
-
 # Display an operation menu to the user
 print('What do you want to do?')
 print('1. Add')
@@ -13,18 +12,21 @@ print('')
 # capture the operation input from the user
 operation = int(input('Type 1/2/3/4\n'))
 
+# capture users 1st and 2nd number to perform math on
+num1 = int(input('Enter first number\n'))
+num2 = int(input('Enter second number\n'))
 
 # check the operation input from the user and perform the math
-while True
-    if operation == 1:
-        num1 + num2
-    elif operation == 2:
-        num1 - num2
-    elif operation == 3:
-        num1 * num2
-    elif operation == 4:
-        num1 / num2
-    return
+if operation == 1:
+    result = num1 + num2
+    print(result)
+elif operation == 2:
+    result = num1 - num2
+    print(result)
+elif operation == 3:
+    result = num1 * num2
+    print(result)
+elif operation == 4:
+    result = num1 / num2
+    print(result)
 
-    else:
-        print('Invalid Choice')
